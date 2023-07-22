9adc46a357ab52470ca4ef73ae7a20f8c6fa225e
lyoma <lyomaikt@gmail.com>
2023-07-22 16:12:08 +0300

    yellow kitchen 13u

---
+++ js/main.js
@@ -0,0 +1,46 @@
+$('.foods').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
+
+
+
+const menuBtn = document.querySelector('.menu__btn');
+const menuNav = document.querySelector('.navbar-menu');
+
+menuBtn.addEventListener('click', ()=>{
+    menuNav.classList.toggle('menu--open');
+});
+
+
+const gap1 = 16;
+
+const carousel1 = document.getElementById("carousel1"),
+  content1 = document.getElementById("content1"),
+  next1 = document.getElementById("next1"),
+  prev1 = document.getElementById("prev1");
+
+next1.addEventListener("click", e1 => {
+  carousel1.scrollBy(width1 + gap1, 0);
+  if (carousel1.scrollWidth !== 0) {
+    prev1.style.display = "flex";
+  }
+  if (content1.scrollWidth - width1 - gap1 <= carousel1.scrollLeft + width1) {
+    next1.style.display = "none";
+  }
+});
+prev1.addEventListener("click", e1 => {
+  carousel1.scrollBy(-(width1 + gap1), 0);
+  if (carousel1.scrollLeft - width1 - gap1 <= 0) {
+    prev1.style.display = "none";
+  }
+  if (!content1.scrollWidth - width1 - gap1 <= carousel1.scrollLeft + width1) {
+    next1.style.display = "flex";
+  }
+});
+
+let width1 = carousel1.offsetWidth;
+window.addEventListener("resize", e1 => (width1 = carousel1.offsetWidth));
+
+
